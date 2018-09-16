2c354cd5f07c690a30b0969e8f69392d0c9e371f
Joanna Marek <asia.marek1@gmail.com>
2018-09-16 17:53:47 +0200

    Update Main.java

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,7 +8,6 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
@@ -18,7 +17,6 @@ public class Main extends Application {
         primaryStage.show();
 
     }
-
 
     public static void main(String[] args) {
         launch(args);
